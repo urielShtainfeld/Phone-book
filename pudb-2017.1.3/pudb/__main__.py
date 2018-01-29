354f462ebdc4d5fbc66e01005576d80a10ed3942
uriel <uriel.shtinfeld@gmail.com>
2018-01-29 10:12:40 +0200

    add to git

--- pudb-2017.1.3/pudb/__main__.py
+++
@@ -1,6 +0,0 @@
-
-from __future__ import absolute_import, division, print_function
-
-if __name__ == "__main__":
-    from pudb.run import main
-    main()
--- pudb-2017.1.3/try-the-debugger.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-
-if test "$1" = ""; then
-  PYINTERP="python"
-else
-  PYINTERP="$1"
-fi
-
-$PYINTERP -m pudb.run debug_me.py
